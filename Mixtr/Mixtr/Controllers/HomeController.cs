8de185e9fc1f37d63457fd1b5cf6d8e2cc2dea36
Daniel Ras <ras98.daniel@gmail.com>
2020-06-01 13:31:33 +0300

    feat: update HomeController Index action to extract data to view model

--- Mixtr/Mixtr/Controllers/HomeController.cs
+++ Mixtr/Mixtr/Controllers/HomeController.cs
@@ -5,12 +5,16 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Data.Entity;
+using System.Web.UI.WebControls;
+using Microsoft.AspNet.Identity;
+using Newtonsoft.Json;
 
 namespace Mixtr.Controllers
 {
     public class HomeController : Controller
     {
         ApplicationDbContext db = new ApplicationDbContext();
+
         public ActionResult Index()
         {
             return View();
@@ -37,26 +41,51 @@ namespace Mixtr.Controllers
         [HttpGet]
         public ActionResult GetPosts()
         {
-            List<Post> listOfPosts;
-
             IEnumerable<Post> posts = db.Posts.Include(p => p.Playlist);
-            listOfPosts = posts.ToList();
 
-            return Json(listOfPosts, JsonRequestBehavior.AllowGet);
+            try
+            {
+                (posts as IQueryable<Post>).Include(p => p.UserWhoPosted);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            List<PostViewModel> listOfPosts = posts.Select(p => new PostViewModel()
+                {
+                    Id = p.Id,
+                    Title = p.Title,
+                    LikesCount = p.LikesCount,
+                    Playlist = p.Playlist,
+                    UserName = p.UserWhoPosted?.UserName
+                }
+            ).ToList();
+
+            try
+            {
+                string json = JsonConvert.SerializeObject(listOfPosts);
+                return Content(json, "application/json");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return new HttpStatusCodeResult(500);
+            }
         }
 
         [Authorize]
         [HttpPost]
         public ActionResult AddPost(Post post)
         {
-
-            if(ModelState.IsValid)
+            if (ModelState.IsValid)
             {
+                post.UserId = User.Identity.GetUserId();
                 db.Posts.Add(post);
                 db.SaveChanges();
-            }            
+            }
 
-            return RedirectToAction("Index", new { status = "success" });
+            return RedirectToAction("Index", new {status = "success"});
         }
     }
 }
