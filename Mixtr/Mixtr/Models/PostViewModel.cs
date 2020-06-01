dd144c21be7c9a6e87832386a5b58ca9fe04e76e
Daniel Ras <ras98.daniel@gmail.com>
2020-06-01 13:28:17 +0300

    feat: add PostViewModel class

---
+++ Mixtr/Mixtr/Models/PostViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Globalization;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Newtonsoft.Json;
+using System.Web.Script.Serialization;
+
+namespace Mixtr.Models
+{
+    public class PostViewModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public int LikesCount { get; set; } = 0;
+        public Playlist Playlist { get; set; }
+        public string UserName { get; set; }
+    }
+}
