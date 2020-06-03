9f8a55011ec4749ca78dafefa629689792eaefa1
Daniel Ras <ras98.daniel@gmail.com>
2020-06-03 09:48:58 +0300

    feat: add AppRepository

---
+++ Mixtr/Mixtr/Interfaces/IAppRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Mixtr.Models
+{
+    public interface IAppRepository : IDisposable
+    {
+        void AddPost(Post p);
+        List<PostViewModel> GetPostsViews();
+        void IncrementLikes(int postId);
+        void DecrementLikes(int postId);
+    }
+}
